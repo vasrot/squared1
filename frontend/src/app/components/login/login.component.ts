76e9d4a1d2f84d48c1350f885fddc9d573fe3859
vasrot <vasrotaru+bitbucket@gmail.com>
2021-07-17 14:27:55 +0200

    Login form working

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  dataLoading: boolean = false;
+  server_response: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.loginForm = this.fb.group({
+      email: [ '', [Validators.required, Validators.minLength(3), Validators.email]],
+      password: [ '', [Validators.required]]
+    });
+    this.server_response = '';
+  }
 
   ngOnInit(): void {
+    this.server_response = '';
+  }
+
+  loginUser() {
+    if (this.loginForm.invalid) { return }
+    this.dataLoading = true;
+    this.authService.login(this.loginForm.value).subscribe(
+      (response:any) => {
+        console.log(response);
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('user_id', response.user.id);
+        this.dataLoading = false;
+        this.router.navigate(['/']);
+      },
+      error => {
+        console.log(error);
+        this.dataLoading = false;
+        this.server_response = error.error;
+      }
+    );
   }
 
 }
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, DoCheck {
+
+  is_auth = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck(): void {
+    if (localStorage.getItem('token') && localStorage.getItem('user_id')) {
+      this.is_auth = true;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user_id");
+    window.location.reload();
   }
 
 }
