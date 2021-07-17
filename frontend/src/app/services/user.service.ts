fd205bd96baf814f5e36a8569dd429863648770f
vasrot <vasrotaru+bitbucket@gmail.com>
2021-07-17 22:02:47 +0200

    User service now has the Authorization header

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,15 +2,18 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private URL = environment.api_url + '/user';
-  private headers = new HttpHeaders().set('Accept', 'application/json');
+  private headers = new HttpHeaders()
+    .set('Accept', 'application/json')
+    .set('Authorization', 'Bearer ' + this.authService.getToken());
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   /**
    *
@@ -23,5 +26,4 @@ export class UserService {
       headers,
     });
   }
-
 }
