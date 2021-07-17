7235c2f1f030db262700553c806d19e5de41c519
vasrot <vasrotaru+bitbucket@gmail.com>
2021-07-17 13:14:41 +0200

    Frontend routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PostComponent } from './components/post/post.component';
+import { PrivateComponent } from './components/private/private.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, pathMatch: 'full'
+  },
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'register', component: RegisterComponent,
+  },
+  {
+    path: 'private', component: PrivateComponent,
+  },
+  {
+    path: 'new-post', component: PostComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
