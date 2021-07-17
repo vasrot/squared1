1287d07afcabf6f4c901bdbe93f4599c8794bda9
vasrot <vasrotaru+bitbucket@gmail.com>
2021-07-17 13:12:16 +0200

    Components added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PrivateComponent } from './components/private/private.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PrivateComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
